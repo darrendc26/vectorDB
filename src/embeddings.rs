7f86f363201f7a7333927254fa8e3ab7844b7dbf
Darren <darren26feb@gmail.com>
2025-10-17 02:41:54 +0530

    Embedding

---
+++ src/embeddings.rs
@@ -0,0 +1,12 @@
+use fastembed::{EmbeddingModel, InitOptions, TextEmbedding};
+
+// With default InitOptions
+
+pub fn get_embeddings(text: Vec<&str>) -> Result<Vec<Vec<f32>>, String> {
+    let mut model = TextEmbedding::try_new(Default::default()).map_err(|e| format!("{:?}", e))?;
+    let embeddings = model
+        .embed(text, Some(1000))
+        .map_err(|e| format!("{:?}", e))?;
+
+    Ok(embeddings)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
 mod chunker;
+mod embeddings;
 use chunker::chunk_text;
+use embeddings::get_embeddings;
 
 fn main() {
-    let text = "Hello, world! This is a test. How are you? I'm doing well, thank you for asking. I'm glad to be here. Let's get started. First, we need to install the Rust programming language. To do that, we need to install the Rust toolchain. The Rust toolchain includes the Rust compiler, package manager, and other tools we'll need to build Rust projects. We'll also need to set up our development environment. Once we have the toolchain installed, we can use it to build and run Rust programs. Let's start by installing the Rust toolchain. Open your terminal and run the following command:";
-    let chunks = chunk_text(text, 10, 2).unwrap();
+    let text = "Hello, world! Thidg imhtoekmmmmmmmmmmmmmmmmm['teohpmt toph[itjeeeeeeeeeeeeeeeeeeeee heoph noern ei n ijrn geri oei rgjjjjjjjjjjb rieeeeeeeeeeeeeeeeeeeeeeeeeeeeeejgn rieongrjifns wslf wibn wi4b";
+    let chunks = chunk_text(text, 100, 2).unwrap();
+    let chunk_slices: Vec<&str> = chunks.iter().map(|s| s.as_str()).collect();
 
+    let embeddings = get_embeddings(chunk_slices).unwrap();
     println!("{:?}", chunks);
+    println!("{:?}", embeddings);
 }
